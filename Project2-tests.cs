8977c5ab62c12fb0f74e64692f69ffc929f7e1e1
John Reiley <rei15006@byui.edu>
2019-07-02 16:51:49 -0600

    completed test for json generator

--- Project2-tests.cs
+++ Project2-tests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using Xunit;
 using AirNomadHttpGrabers;
 using ReportGeneratorFunctions;
@@ -11,27 +12,41 @@ namespace ReportGenerator
     public class JSONTests
     {
         [Fact]
-        public void Test1()
+        public async void ActualAndExpectedShouldBeEqual()
         {
-            
+            var compiler = new ReportCompile();
+            var http = new CourseGrabber();
+            var promptObj = new Prompt("59796", "JSON", "../../../../air-nomads-project-2-testz/generated.json");
+            http.CourseID = promptObj.CourseId;
+            compiler.CalibrateCompiler(promptObj, new GenerateJSON(promptObj.Destination), http);
+            var success = await compiler.CompileReport();
+            // System.Console.WriteLine(success);
+
+            if (success)
+            {
+                var actual = File.ReadAllText("../../../../air-nomads-project-2-testz/generated.json");
+                var expected = File.ReadAllText("../../../../air-nomads-project-2-testz/expected.json");
+
+                Assert.Equal(expected, actual);
+            }
         }
     }
 
-     public class CSVTests
+    public class CSVTests
     {
         [Fact]
         public void Test1()
         {
-            
+
         }
     }
 
-     public class HTMLTests
+    public class HTMLTests
     {
         [Fact]
         public void Test1()
         {
-            
+
         }
     }
 }
